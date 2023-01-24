0613c08fdc351f434135d52c625e58386ad2974c
LukeHoney <2248102@swansea.ac.uk>
2023-01-24 18:24:34 +0000

    Luke: Lighting Baked, Added higher quality assets, added audio for water and scissors and coding completed

--- Assets/Luke/Script/WaterEffect.cs
+++ Assets/Luke/Script/WaterEffect.cs
@@ -1,33 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WaterEffect : MonoBehaviour
 {
     public GameObject wateringCan;
+    public ParticleSystem waterParticles;
+    public InputActionReference thrustAction;
+    public GameObject waternoise;
 
+    public bool plantArea;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        if (wateringCan.transform.rotation.eulerAngles.x <= -140f)
-        {
-            Debug.Log("Water is free");
-        }
-
-
+        waterParticles.Stop();
+        waternoise.SetActive(false);
+        plantArea = false;
     }
-    public void EndGameOnDegrees()
+
+    private void OnTriggerEnter(Collider other)
     {
-        float min = -140f;
-        float max = -200f;
-
-        float angle = Vector3.Angle(Vector3.up, transform.up);
-
-
-        if (angle < min || angle > max)
+        if (other.tag == "PlantArea")
         {
-            Debug.Log("Water is free EndGame");
+            plantArea = true;
+            Debug.Log("In water area");
         }
     }
+
+    private void Update()
+    {
+        if (thrustAction.action.IsPressed())
+        {
+            if (plantArea == true)
+            {
+                waterParticles.Play();
+                waternoise.SetActive(true);
+            }
+            
+        }
+        else
+        {
+            waterParticles.Stop();
+            waternoise.SetActive(false);
+        }
+    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (wateringCan.transform.rotation.eulerAngles.x <= -140f)
+    //    {
+    //        Debug.Log("Water is free");
+    //    }
+
+
+    //}
+    //public void EndGameOnDegrees()
+    //{
+    //    float min = -140f;
+    //    float max = -200f;
+
+    //    float angle = Vector3.Angle(Vector3.up, transform.up);
+
+
+    //    if (angle < min || angle > max)
+    //    {
+    //        Debug.Log("Water is free EndGame");
+    //    }
+    //}
 }
