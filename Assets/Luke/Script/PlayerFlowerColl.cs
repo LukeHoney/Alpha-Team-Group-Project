86493fcf069f05a28e161d5e5688482d632ab436
LukeHoney <2248102@swansea.ac.uk>
2023-01-25 22:00:17 +0000

    Luke: Added Background, trigger area for audio and code to activate audio.

---
+++ Assets/Luke/Script/PlayerFlowerColl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlowerColl : MonoBehaviour
+{
+    
+    public GameObject plantNarration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plantNarration.SetActive(false);
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            plantNarration.SetActive(true);
+            Debug.Log("flower talk");
+        }
+    }
+}
